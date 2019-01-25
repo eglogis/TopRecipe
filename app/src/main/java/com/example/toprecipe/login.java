aa13be9ac3857367a6cd4bc5c5903ccd5cb8113c
eglogis <samu6333@gmail.com>
2019-01-25 18:59:42 +0100

    revision 2
    Layout de login acabado

--- app/src/main/java/com/example/toprecipe/login.java
+++ app/src/main/java/com/example/toprecipe/login.java
@@ -1,7 +1,10 @@
 package com.example.toprecipe;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        hacerInvisibleStatusBar();
+    }
+
+    private void hacerInvisibleStatusBar(){
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
